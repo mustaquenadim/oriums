a2ad28c20f91ec179c8f076659cc76284510efc6
mustaquenadim <mustaquenadim2000@gmail.com>
2026-01-04 01:08:17 +0300

    ♻️ Update ThemeProvider to use forced dark theme and disable system theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <body className='font-sans antialiased'>
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
-          enableSystem
+          forcedTheme='dark'
+          enableSystem={false}
           disableTransitionOnChange
         >
           <Header />
